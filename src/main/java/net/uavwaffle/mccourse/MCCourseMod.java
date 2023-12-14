0d8462ba3ec4bd1912b51ea90ee1de39b0ba408a
UAVwaffle <68166007+UAVwaffle@users.noreply.github.com>
2023-12-14 11:52:40 -0500

    added a comment

--- src/main/java/net/uavwaffle/mccourse/MCCourseMod.java
+++ src/main/java/net/uavwaffle/mccourse/MCCourseMod.java
@@ -2,6 +2,7 @@ package net.uavwaffle.mccourse;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.uavwaffle.mccourse.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,7 +12,6 @@ public class MCCourseMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		//Bro this comment is sick!
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
--- src/main/java/net/uavwaffle/mccourse/item/ModItems.java
+++ src/main/java/net/uavwaffle/mccourse/item/ModItems.java
@@ -1,7 +1,10 @@
 package net.uavwaffle.mccourse.item;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -9,10 +12,20 @@ import net.uavwaffle.mccourse.MCCourseMod;
 
 public class ModItems {
 
+    public static final Item PINK_GARNET = registerItem("pink_garnet",
+            new Item(new FabricItemSettings()));
+
     public static Item registerItem(String name, Item item){
         return Registry.register(Registries.ITEM, new Identifier(MCCourseMod.MOD_ID, name), item);
     }
-    private static void itemGroupIngredients(FabricItemGroupEntries entries){
+    private static void itemGroupIngredients(FabricItemGroupEntries entries){//All items in here get put in the ingredients tab, make a new one and a new ItemGroupEvent to put them in other creative tabs
+        entries.add(PINK_GARNET);
+    }
 
+
+    public static void registerModItems(){
+        MCCourseMod.LOGGER.info("Registering Mod Items for: " + MCCourseMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::itemGroupIngredients);
     }
 }
