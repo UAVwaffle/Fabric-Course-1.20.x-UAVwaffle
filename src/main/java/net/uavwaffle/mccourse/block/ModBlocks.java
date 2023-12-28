a06a7fb2dcf8dc15885037b0933e3146ef0307d9
UAVwaffle <68166007+UAVwaffle@users.noreply.github.com>
2023-12-28 18:06:22 -0500

    Added a bunch of pink garnet recipes and drops. You can mine the pink garnet now

--- src/main/java/net/uavwaffle/mccourse/block/ModBlocks.java
+++ src/main/java/net/uavwaffle/mccourse/block/ModBlocks.java
@@ -4,11 +4,13 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.uavwaffle.mccourse.MCCourseMod;
 
 public class ModBlocks {
@@ -20,13 +22,13 @@ public class ModBlocks {
 
                                 /*Pink Garnet Ore Pack*/
     public static final Block PINK_GARNET_ORE = registerBlock("pink_garnet_ore",
-            new Block(FabricBlockSettings.copyOf(Blocks.EMERALD_ORE)));
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.EMERALD_ORE), UniformIntProvider.create(10,20)));
     public static final Block DEEPSLATE_PINK_GARNET_ORE = registerBlock("deepslate_pink_garnet_ore",
-            new Block(FabricBlockSettings.copyOf(Blocks.DEEPSLATE_EMERALD_ORE)));
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.DEEPSLATE_EMERALD_ORE), UniformIntProvider.create(10,20)));
     public static final Block END_STONE_PINK_GARNET_ORE = registerBlock("end_stone_pink_garnet_ore",
-            new Block(FabricBlockSettings.copyOf(Blocks.DEEPSLATE_EMERALD_ORE)));
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.DEEPSLATE_EMERALD_ORE), UniformIntProvider.create(10,20)));
     public static final Block NETHER_PINK_GARNET_ORE = registerBlock("nether_pink_garnet_ore",
-            new Block(FabricBlockSettings.copyOf(Blocks.NETHER_GOLD_ORE)));
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.NETHER_GOLD_ORE), UniformIntProvider.create(10,20)));
 
 
     private static Block registerBlock(String name, Block block){
